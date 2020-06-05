b0602da7dc8a9e4810be966ca1057e34138a5a40
samcode31 <37969114+samcode31@users.noreply.github.com>
2020-06-04 22:52:35 -0400

    no authentication

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@
 |
 */
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', 'HomeController@welcome');
-Route::get('/signin', 'AuthController@signin');
-Route::get('/callback', 'AuthController@callback');
-Route::get('/signout', 'AuthController@signout');
+Route::get('/', function () {
+    return view('welcome');
+});
