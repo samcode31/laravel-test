dce719190687e8dad00de4c6a85b8770c2c80041
samcode31 <37969114+samcode31@users.noreply.github.com>
2020-06-05 01:24:18 -0400

    microsoft signin

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'HomeController@welcome');
+Route::get('/signin', 'AuthController@signin');
+Route::get('/callback', 'AuthController@callback');
+Route::get('/signout', 'AuthController@signout');
